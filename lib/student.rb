e90cd3213179535c854937d052184cfca7a22cb8
Roberto Samia <bojosteph@gmail.com>
2019-01-17 03:08:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,6 +63,19 @@ class Student
   end
   
   def self.first_X_students_in_grade_10
+    sql = <<-SQL
+      SELECT * 
+      FROM students
+      WHERE students.grade = 10
+      LIMIT 10
+    SQL
+    
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row
+    end
+    
+    
+  end
   
   
   def save
