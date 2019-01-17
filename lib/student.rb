43368c256823d1abe89899ec2952c653c59360c6
Roberto Samia <bojosteph@gmail.com>
2019-01-17 02:20:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,9 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    student = Steudent.new(name, grade)
+    student.save
+    student
   end
 
   def self.all
