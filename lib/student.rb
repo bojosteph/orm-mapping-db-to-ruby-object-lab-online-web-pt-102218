263dc33507a6f58aff844bbf2e1377e985a15df4
Roberto Samia <bojosteph@gmail.com>
2019-01-17 03:22:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,12 +62,12 @@ class Student
     end
   end
   
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
       SELECT * 
       FROM students
       WHERE students.grade = 10
-      LIMIT = ?
+      LIMIT = x
     SQL
     
     DB[:conn].execute(sql).collect do |row|
